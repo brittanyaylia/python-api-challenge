7b7c1faa7d825f9228456d88e4d78220040f1b95
brittanyaylia <whitakerbrittany18@gmail.com>
2021-03-20 14:18:34 -0700

    api

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "87734b995d714e3fb904706a4dba60ff"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB4PLufVpWK3PnxxNOptCi9RU3i1ou7rqU"
